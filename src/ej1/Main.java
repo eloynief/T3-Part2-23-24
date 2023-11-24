f01f2f3eb7d824c3b1f0293dd410c8855bfa35f9
enieves <enieves@106-02.info2>
2023-11-24 14:28:24 +0100

    testa

---
+++ src/ej1/Main.java
@@ -0,0 +1,97 @@
+package ej1;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//creación de scanner
+		Scanner sc=new Scanner(System.in);
+		
+		
+		
+		
+
+		//cierre de scanner
+		sc.close();
+		
+	}
+	
+	//ej1
+	
+	//a)
+	/**
+	 * funcion que devuelve la suma de 2 numeros int
+	 * @param n1
+	 * @param n2
+	 * @return
+	 */
+	int sumaArgsInt(int n1,int n2) {
+		int suma;
+		
+		
+		suma=n1+n2;
+		return suma;
+	}
+	
+
+	//b)
+	/**
+	 * funcion que devuelve la suma de 2 numeros double
+	 * @param n1
+	 * @param n2
+	 * @return
+	 */
+	double sumaArgsDouble(double n1,double n2) {
+		double suma;
+		
+		
+		suma=n1+n2;
+		return suma;
+	}
+	
+	
+	
+	
+	
+	//ej2
+	
+	//a)
+	/**
+	 * funcion que devuelve la media de 2 numeros int
+	 * @param n1
+	 * @param n2
+	 * @return
+	 */
+	double mediaArgsDouble(int n1,int n2) {
+		double media;
+		
+		
+		media=n1*n2/2;
+		return media;
+	}
+	
+
+	//b)
+	/**
+	 * funcion que devuelve la media de 3 numeros int
+	 * @param n1
+	 * @param n2
+	 * @return
+	 */
+	double mediaArgsDouble(int n1,int n2,int n3) {
+		double media;
+		
+		
+		media=n1*n2*n3/3;
+		return media;
+	}
+	
+	
+	//ej3
+	
+	
+	
+
+}
---
+++ src/ej5/Main.java
@@ -0,0 +1,21 @@
+package ej5;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//creación de scanner
+		Scanner sc=new Scanner(System.in);
+		
+		
+		
+		
+
+		//cierre de scanner
+		sc.close();
+		
+	}
+
+}
